d9eafddd83b2973d662b87aa592726cf2fa2437a
d3ad connection <86633642+d3adconnection@users.noreply.github.com>
2024-11-22 22:18:47 -0600

    Update specialize.ps1
    Disable any sleep functions during specialize

--- sources/$OEM$/$$/Setup/Scripts/specialize.ps1
+++ sources/$OEM$/$$/Setup/Scripts/specialize.ps1
@@ -10,6 +10,16 @@ $highPerfGuid = powercfg -duplicatescheme 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c |
 
 # Activate the temporary "High Performance" plan
 powercfg -setactive $highPerfGuid
+
+# Disable any sleep functions on this temporary power plan
+powercfg /x -hibernate-timeout-ac 0
+powercfg /x -hibernate-timeout-dc 0
+powercfg /x -disk-timeout-ac 0
+powercfg /x -disk-timeout-dc 0
+powercfg /x -monitor-timeout-ac 0
+powercfg /x -monitor-timeout-dc 0
+powercfg /x -standby-timeout-ac 0
+powercfg /x -standby-timeout-dc 0
 ###########################################
 
 ###########################################
