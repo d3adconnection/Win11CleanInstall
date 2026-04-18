60e07509ca891aa9cde32392bb829af176ec7af1
d3ad connection <86633642+d3adconnection@users.noreply.github.com>
2026-04-17 23:02:09 -0500

    Update setupcomplete.cmd
    Add comments to match other scripts

--- sources/$OEM$/$$/Setup/Scripts/setupcomplete.cmd
+++ sources/$OEM$/$$/Setup/Scripts/setupcomplete.cmd
@@ -1,3 +1,11 @@
+:: :: :::::::::::::::::::: :: :: ::::::::::
+: : WINDOWS 11 CLEAN INSTALL : : :::::::::
+:::::::::::::::::::::::::::::::::::::::::::
+ ::::::: packaged by d3ad connection :::::
+
+:: Run setupcomplete.ps1
 cd /d %~dp0
 if exist setupcomplete.ps1 start /wait powershell -ex bypass -nol -noni -nop -f setupcomplete.ps1
+
+:: Delete self
 (goto) 2>nul & del "%~f0"
